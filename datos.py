2a9750c8dd4019a709fca55c0d7b15e1efdee483
TuNombreDeUsuario <TuCorreo@ejemplo.com>
2025-03-08 20:27:19 -0600

    atuliacion67

--- datos.py
+++ datos.py
@@ -1,4 +1,9 @@
 import streamlit as st
 
 uploaded_file = st.file_uploader("📂 Sube tu archivo Excel", type=["xlsx"])
+if uploaded_file is not None:
+    st.success("✅ Archivo subido correctamente")
+else:
+    st.error("❌ No se ha subido ningún archivo")
+
 
